6e859134e650c369eda3cf9138da924380772013
Kouichi OHTA <kouichi.ohta@gmail.com>
2017-02-01 00:07:10 +0900

    add library: Timber

--- app/src/main/java/jp/aknot/retromo/MyApp.java
+++ app/src/main/java/jp/aknot/retromo/MyApp.java
@@ -4,10 +4,16 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
+import timber.log.Timber;
+
 public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
         Stetho.initializeWithDefaults(this);
     }
 }
